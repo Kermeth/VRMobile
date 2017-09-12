5a312902fd3b75d64392c3c1a8d51cec45e4b4e9
Cyberdev <marcestupina@gmail.com>
2017-09-13 00:10:05 +0200

    Intro Scene

---
+++ Assets/Code/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class GameManager : Singleton<GameManager> {
+
+	protected GameManager () { }
+
+    public void SetVR()
+    {
+        VRSettings.LoadDeviceByName("cardboard");
+        VRSettings.enabled = true;
+    }
+
+    public void Set360()
+    {
+        VRSettings.LoadDeviceByName("cardboard");
+        VRSettings.enabled = false;
+    }
+
+}
---
+++ Assets/Code/Util.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Util : MonoBehaviour {
+
+	public void PlayVR()
+    {
+        GameManager.Instance.SetVR();
+    }
+}
